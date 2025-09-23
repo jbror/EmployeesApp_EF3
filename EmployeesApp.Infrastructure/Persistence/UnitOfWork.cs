297a015a5ffe00e16c38bc84fe01f2f353090c69
bror <reachingbror@gmail.com>
2025-09-23 12:22:06 +0200

    Add UnitOfWork class for managing repositories
    A new `UnitOfWork` class has been introduced in the `EmployeesApp.Infrastructure.Persistence` namespace. This class includes readonly fields for `ApplicationContext`, `ICompanyRepository`, and `IEmployeeRepository`, which are initialized via dependency injection in the constructor.
    
    Additionally, several `using` directives were added to include necessary namespaces for the implementation.
    
    Working on my UnitOfWork implementation

---
+++ EmployeesApp.Infrastructure/Persistence/UnitOfWork.cs
@@ -0,0 +1,39 @@
+﻿using EmployeesApp.Application.Companies.Interfaces;
+using EmployeesApp.Application.Employees.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeesApp.Infrastructure.Persistence;
+
+public class UnitOfWork
+{
+
+    public readonly ApplicationContext _context;
+    public readonly ICompanyRepository _companyRepository;
+    public readonly IEmployeeRepository _employeeRepository;
+
+
+    public UnitOfWork(ApplicationContext context,
+                      ICompanyRepository companyRepository,
+                      IEmployeeRepository employeeRepository)
+    {
+        _context = context;
+        _companyRepository = companyRepository;
+        _employeeRepository = employeeRepository;
+
+
+
+    }
+
+
+
+    
+
+
+
+
+
+}
